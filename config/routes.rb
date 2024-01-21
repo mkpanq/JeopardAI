b63895534184201a9e60dfa007e53a78f4911be4
Marek Pankowski <17934750+mkpanq@users.noreply.github.com>
2024-01-21 21:46:46 +0100

    Hide sidekiq behind basic login

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,16 @@ require 'sidekiq/web'
 require 'sidekiq-scheduler/web'
 
 Rails.application.routes.draw do
-  # TODO: Make sidekiq secure behind a login
+  Sidekiq::Web.use(Rack::Auth::Basic) do |username, password|
+      ActiveSupport::SecurityUtils.secure_compare(
+        ::Digest::SHA256.hexdigest(username),
+        ::Digest::SHA256.hexdigest(Rails.application.credentials.dig(:sidekiq_creds, :username))
+      ) &
+      ActiveSupport::SecurityUtils.secure_compare(
+        ::Digest::SHA256.hexdigest(password),
+        ::Digest::SHA256.hexdigest(Rails.application.credentials.dig(:sidekiq_creds, :password))
+      )
+  end
   mount Sidekiq::Web => "/sidekiq"
 
   root "home#home"
