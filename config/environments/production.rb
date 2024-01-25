ca3f23ec986b1e459a591b6e4d449660b6c56a76
Marek Pankowski <17934750+mkpanq@users.noreply.github.com>
2024-01-25 17:17:21 +0100

    Perform JOb after image initialization

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.after_initialize do
+    DailyGenerationJob.perform_later
+  end
 end
